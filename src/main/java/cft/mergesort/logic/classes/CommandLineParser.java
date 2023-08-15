62e64a5c63bf1f46b63216e17879e9bf965aa226
Effectuss <panovsavelii@gmail.com>
2023-08-15 18:27:13 +0700

    Parsing Command line sort mode ready

--- src/main/java/cft/mergesort/logic/classes/CommandLineParser.java
+++ src/main/java/cft/mergesort/logic/classes/CommandLineParser.java
@@ -2,14 +2,53 @@ package cft.mergesort.logic.classes;
 
 import cft.mergesort.logic.enums.DataType;
 import cft.mergesort.logic.enums.SortMode;
+import com.beust.jcommander.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommandLineParser {
-    private SortMode sortMode = SortMode.ASCENDING;
-    private DataType dataType;
-    private String outFileName;
-    private String[] inputFileName;
 
-    public CommandLineParser(String[] args) {
+    @Parameter(names = {"-a"}, arity = 0, description = "Sort ascending mode.", validateWith = UniqueOptionValidator.class)
+    private boolean ascending;
 
+    @Parameter(names = {"-d"}, arity = 0, description = "Sort descending mode.", validateWith = UniqueOptionValidator.class)
+    private boolean descending;
+
+    @Parameter(names = {"-s", "-i"}, arity = 0, description = "Type of data for sort")
+    private boolean dataType;
+
+    private static CommandLineParser instance;
+
+    public CommandLineParser() {
+        instance = this;
+    }
+
+    public static CommandLineParser getInstance() {
+        return instance;
+    }
+
+    public SortMode getSortMode() {
+        if (ascending) {
+            return SortMode.ASCENDING;
+        } else if (descending) {
+            return SortMode.DESCENDING;
+        }
+        return SortMode.ASCENDING;
+    }
+
+    public static class UniqueOptionValidator implements IParameterValidator {
+        @Override
+        public void validate(String name, String value) throws ParameterException {
+            if (name.equals("-a") && CommandLineParser.getInstance().descending) {
+                throw new ParameterException("Option -a cannot be used together with option -d");
+            } else if (name.equals("-d") && CommandLineParser.getInstance().ascending) {
+                throw new ParameterException("Option -d cannot be used together with option -a");
+            } else if (name.equals("-a") && CommandLineParser.getInstance().ascending) {
+                throw new ParameterException("Option -a cannot be used together with option -a");
+            } else if (name.equals("-d") && CommandLineParser.getInstance().descending) {
+                throw new ParameterException("Option -d cannot be used together with option -d");
+            }
+        }
     }
 }
--- src/main/java/cft/mergesort/main/Main.java
+++ src/main/java/cft/mergesort/main/Main.java
@@ -3,11 +3,23 @@ package cft.mergesort.main;
 import cft.mergesort.logic.classes.CommandLineParser;
 import cft.mergesort.logic.classes.FileMerger;
 
+import com.beust.jcommander.JCommander;
+import org.apache.commons.cli.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        FileMerger fileMerger = new FileMerger();
-        CommandLineParser parser = new CommandLineParser(args);
-        System.out.println("Hello World!");
+        try {
+            FileMerger fileMerger = new FileMerger();
+            CommandLineParser CommandLineParser = new CommandLineParser();
+            JCommander.newBuilder()
+                    .addObject(CommandLineParser)
+                    .build()
+                    .parse(args);
+
+            System.out.println(CommandLineParser.getSortMode());
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
