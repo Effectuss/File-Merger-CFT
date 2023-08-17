2adbe0fecc7446e7bec73bba2442223bd57668ed
Effectuss <panovsavelii@gmail.com>
2023-08-17 20:29:12 +0700

    Finish method read first line and validation

--- src/main/java/cft/mergesort/logic/classes/SortedFileMerger.java
+++ src/main/java/cft/mergesort/logic/classes/SortedFileMerger.java
@@ -1,15 +1,20 @@
 package cft.mergesort.logic.classes;
 
+import cft.mergesort.logic.enums.DataType;
 import cft.mergesort.logic.interfaces.FileMerger;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class SortedFileMerger implements FileMerger {
     private SortConfiguration configuration;
+    private List<String> currentLine;
+    private List<String> previousLine;
+    private List<Boolean> stopAlgorithm;
 
     public SortedFileMerger(SortConfiguration configuration) {
         this.setSortConfiguration(configuration);
@@ -24,15 +29,48 @@ public class SortedFileMerger implements FileMerger {
             return;
         }
 
-        List<String> currentLine = new ArrayList<>(readers.size());
-        List<String> previousLine = new ArrayList<>(readers.size());
+        currentLine = new ArrayList<>(Collections.nCopies(readers.size(), null));
+        previousLine = new ArrayList<>(Collections.nCopies(readers.size(), null));
+        stopAlgorithm = new ArrayList<>(Collections.nCopies(readers.size(), false));
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.configuration.getOutputFile()))) {
+            readFirstLineEachFile(readers);
+            for (String s : currentLine) {
+                if (s != null) {
+                    writer.write(s);
+                    writer.newLine();
+                }
+            }
         } finally {
             closeReaders(readers);
         }
     }
 
+    private void readFirstLineEachFile(List<BufferedReader> readers) throws IOException {
+        for (int i = 0; i < readers.size(); ++i) {
+            String firstLine = readers.get(i).readLine();
+            if (isValidData(firstLine, i)) {
+                currentLine.add(i, firstLine);
+                previousLine.add(i, firstLine);
+            }
+        }
+    }
+
+    private boolean isValidData(String line, int index) {
+        if (line.contains(" ") || line.isEmpty()) {
+            return false;
+        } else if (configuration.getDataType() == DataType.INTEGER) {
+            try {
+                Integer.parseInt(line);
+                return true;
+            } catch (NumberFormatException e) {
+                System.err.println("Data cant be number");
+                stopAlgorithm.add(index, true);
+                return false;
+            }
+        }
+        return true;
+    }
 
     private List<BufferedReader> createReaders() {
         return configuration.getInputFiles().stream()
