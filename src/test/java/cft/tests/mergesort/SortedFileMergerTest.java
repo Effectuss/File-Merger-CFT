90aca3d17ec90bc3382d3a25706d6e2d8f5455ec
Effectuss <panovsavelii@gmail.com>
2023-08-19 20:43:53 +0700

    Test refactoring done

--- src/test/java/cft/tests/mergesort/SortedFileMergerTest.java
+++ src/test/java/cft/tests/mergesort/SortedFileMergerTest.java
@@ -5,6 +5,7 @@ import cft.mergesort.logic.classes.SortedFileMerger;
 import cft.mergesort.logic.enums.DataType;
 import cft.mergesort.logic.enums.SortMode;
 import cft.mergesort.logic.interfaces.FileMerger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -18,354 +19,117 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SortedFileMergerTest {
-    private static final URL wayToRes = CommandLineParserTest.class
-            .getClassLoader()
-            .getResource("test_resources_sorted_file_merger" + File.separator);
+    private static final String TEST_RESOURCES_PATH = "test_resources_sorted_file_merger";
+    private static final String CASE_PREFIX = "case";
 
-    private String testCase;
+    private URL testResourcesURL;
 
-    @Test
-    void testSortedFileMergerCorrectIntegerAscending() throws IOException {
-        testCase = wayToRes.getPath() + "case1" + File.separator;
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
+    @BeforeEach
+    void setUp() {
+        testResourcesURL = CommandLineParserTest.class.getClassLoader().getResource(TEST_RESOURCES_PATH + File.separator);
+    }
+
+    private void runAndAssertMerge(String testCasePath, DataType dataType, SortMode sortMode) throws IOException {
+        File testCaseFolder = new File(testCasePath);
+        File testFile = new File(testCaseFolder, "out.txt");
+        File outputFile = new File(testCaseFolder, "test.txt");
 
         List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
+                new File(testCaseFolder, "in1.txt"),
+                new File(testCaseFolder, "in2.txt"),
+                new File(testCaseFolder, "in3.txt")
         ));
 
-        SortConfiguration configuration = new SortConfiguration(DataType.INTEGER,
-                SortMode.ASCENDING,
-                outputFile, inputFiles);
+        SortConfiguration configuration = new SortConfiguration(dataType, sortMode, outputFile, inputFiles);
         FileMerger fileMerger = new SortedFileMerger(configuration);
         fileMerger.merge();
 
         String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
         String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
         assertEquals(fileContent1, fileContent2);
+    }
+    @Test
+    void testSortedFileMergerCorrectIntegerAscending() throws IOException {
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "1" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.INTEGER, SortMode.ASCENDING);
     }
 
     @Test
     void testSortedFileMergerCorrectStringAscending() throws IOException {
-        testCase = wayToRes.getPath() + "case2" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.STRING,
-                SortMode.ASCENDING,
-                outputFile, inputFiles);
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "2" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.STRING, SortMode.ASCENDING);
     }
 
     @Test
     void testSortedFileMergerCorrectIntegerDescending() throws IOException {
-        testCase = wayToRes.getPath() + "case3" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.INTEGER,
-                SortMode.DESCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "3" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.INTEGER, SortMode.DESCENDING);
     }
 
     @Test
     void testSortedFileMergerWithBadOrderStringDescending() throws IOException {
-        testCase = wayToRes.getPath() + "case4" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.STRING,
-                SortMode.DESCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "4" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.STRING, SortMode.DESCENDING);
     }
 
     @Test
     void testSortedFileMergerStringWithSpace() throws IOException {
-        testCase = wayToRes.getPath() + "case5" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.STRING,
-                SortMode.ASCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "5" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.STRING, SortMode.ASCENDING);
     }
 
     @Test
     void testSortedFileMergerIntegerWithSpace() throws IOException {
-        testCase = wayToRes.getPath() + "case6" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.INTEGER,
-                SortMode.ASCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "6" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.INTEGER, SortMode.ASCENDING);
     }
 
     @Test
     void testSortedFileMergerIntegerWithPartialSortAscending() throws IOException {
-        testCase = wayToRes.getPath() + "case7" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.INTEGER,
-                SortMode.ASCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "7" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.INTEGER, SortMode.ASCENDING);
     }
 
     @Test
     void testSortedFileMergerIntegerWithPartialSortDescending() throws IOException {
-        testCase = wayToRes.getPath() + "case8" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.INTEGER,
-                SortMode.DESCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "8" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.INTEGER, SortMode.DESCENDING);
     }
 
     @Test
     void testSortedFileMergerStringWithPartialSortDescending() throws IOException {
-        testCase = wayToRes.getPath() + "case9" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.STRING,
-                SortMode.DESCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "9" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.STRING, SortMode.DESCENDING);
     }
 
     @Test
     void testSortedFileMergerStringWithPartialSortAscending() throws IOException {
-        testCase = wayToRes.getPath() + "case10" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.STRING,
-                SortMode.ASCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "10" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.STRING, SortMode.ASCENDING);
     }
 
     @Test
     void testSortedFileMergerWithBadDataTypeForIntegerAscending() throws IOException {
-        testCase = wayToRes.getPath() + "case11" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.INTEGER,
-                SortMode.ASCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "11" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.INTEGER, SortMode.ASCENDING);
     }
 
     @Test
     void testSortedFileMergerWithBadDataTypeForIntegerDescending() throws IOException {
-        testCase = wayToRes.getPath() + "case12" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.INTEGER,
-                SortMode.DESCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "12" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.INTEGER, SortMode.DESCENDING);
     }
 
     @Test
     void testSortedFileMergerWithBadDataTypeForStringAscending() throws IOException {
-        testCase = wayToRes.getPath() + "case13" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.STRING,
-                SortMode.ASCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "13" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.STRING, SortMode.ASCENDING);
     }
 
     @Test
     void testSortedFileMergerWithBadDataTypeForStringDescending() throws IOException {
-        testCase = wayToRes.getPath() + "case14" + File.separator;
-
-        File testFile = new File(testCase + "out.txt");
-        File outputFile = new File(testCase + "test.txt");
-
-        List<File> inputFiles = new ArrayList<>(Arrays.asList(
-                new File(testCase + "in1.txt"),
-                new File(testCase + "in2.txt"),
-                new File(testCase + "in3.txt")
-        ));
-
-        SortConfiguration configuration = new SortConfiguration(DataType.STRING,
-                SortMode.DESCENDING,
-                outputFile, inputFiles);
-
-        FileMerger fileMerger = new SortedFileMerger(configuration);
-        fileMerger.merge();
-
-        String fileContent1 = new String(Files.readAllBytes(testFile.toPath()));
-        String fileContent2 = new String(Files.readAllBytes(outputFile.toPath()));
-        assertEquals(fileContent1, fileContent2);
+        String testCasePath = testResourcesURL.getPath() + CASE_PREFIX + "14" + File.separator;
+        runAndAssertMerge(testCasePath, DataType.STRING, SortMode.DESCENDING);
     }
+
 }
